20f86198ee6990e2f1dd20ff5f6ff80cc0b39204
/home/ihavesoreeyes <goodmost03@gmail.com>
2026-08-06 18:28:51 +0800

    Update install-test.sh
    im just tired

--- install-test.sh
+++ install-test.sh
@@ -39,7 +39,9 @@ echo "[+] Extracting rootfs into $FEDORA_DIR"
 mkdir -p "$FEDORA_DIR"
 tar -xf "$TARBALL" -C "$FEDORA_DIR" --overwrite 2>/dev/null || true
 
+mkdir -p "$FEDORA_DIR/etc"
 rm -f "$FEDORA_DIR/etc/resolv.conf"
+mkdir -p "$FEDORA_DIR/etc/resolv.conf"
 echo "nameserver 8.8.8.8" > "$FEDORA_DIR/etc/resolv.conf"
 echo "nameserver 1.1.1.1" >> "$FEDORA_DIR/etc/resolv.conf"
 
