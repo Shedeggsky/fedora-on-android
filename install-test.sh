d1858fc4c5116ec7e1356fa4b9e8c2c2d472efac
/home/ihavesoreeyes <goodmost03@gmail.com>
2026-08-06 18:16:43 +0800

    please
    please

--- install-test.sh
+++ install-test.sh
@@ -37,7 +37,7 @@ mkdir -p "$FEDORA_DIR"
 
 echo "[+] Extracting rootfs into $FEDORA_DIR"
 mkdir -p "$FEDORA_DIR"
-tar -xf "$HOME/rootfs.tar.xz" -C "$FEDORA_DIR" --overwrite --exclude='dev' || true
+tar -xf "$TARBALL" -C "$FEDORA_DIR" --overwrite --hard-dereference --exclude='dev' --exclude='proc' --exclude='sys'
 
 echo "nameserver 8.8.8.8" > "$FEDORA_DIR/etc/resolv.conf"
 echo "nameserver 1.1.1.1" >> "$FEDORA_DIR/etc/resolv.conf"
